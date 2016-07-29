5c08dbcadde72864dd5e7cd37d0737877a15d56f
Dave Woods <dwoodscs@gmail.com>
2016-07-29 18:31:22 +0000

    Changed port definition

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.get('/:tag', function(req, res) {
 	res.json(resObj);
 });
 
-app.listen(8080, function() {
-	console.log('The app is listening on port 8080');
+const port = process.env.port || 8080;
+app.listen(port, function() {
+	console.log(`The app is listening on port ${port}`);
 });
